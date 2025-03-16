2104fc2443b4affa66f335dbfb7e1c8e12bc7680
Anupa <peramunaanupa@gmail.com>
2025-03-16 11:08:30 +1100

    Update routes.py to pase output
    - Update 'routes.py' in 'langflow' directory to parse json output and return only the required outputs

--- backend/app/langflow/routes.py
+++ backend/app/langflow/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request
 import os
 from app.langflow.langflow_helper import run_flow
 from dotenv import load_dotenv
+import json
 
 
 langflow_bp = Blueprint('/langflow', __name__, url_prefix='/langflow')
@@ -16,17 +17,9 @@ application_token = os.getenv("APPLICATION_TOKEN")
 @langflow_bp.get("/feedback")
 def langflow_feedback():
     try:
-
-        # user_message = request.json.get('message')
-        # if not user_message:
-        #     return jsonify({'error': 'No message provided'}), 400
-
-        #use runflow
-        # return jsonify({'response': "__runflow__response"})
-
         example_message = (run_flow("I am feeling sad and stressed", endpoint=flow_id, application_token=application_token))
-        print(example_message)
-        return (example_message)
+        output_message  = json.loads(example_message["outputs"][0]["outputs"][0]["messages"][0]["message"])
+        return (output_message)
 
     except Exception as e:
         print(f"Error registering user: {e}") 
